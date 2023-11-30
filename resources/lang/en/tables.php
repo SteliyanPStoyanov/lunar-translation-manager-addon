d52aa978427c405545717f2cc39ef67140937df9
Steliyan Stoyanov <steliyan.p.stoyanov@gmail.com>
2023-11-30 12:17:15 +0200

    improve translation filter list

--- resources/lang/en/tables.php
+++ resources/lang/en/tables.php
@@ -4,4 +4,9 @@ return [
     'headings.title' => 'Translation group',
     'headings.key' => 'Translation key',
     'headings.preview-in-your-lang' => 'Preview in your current language (:lang)',
+    'filters.group' => 'Filter by Group',
+    'filters.text' => 'Filter by language (:lang)',
+    'filters.option.all' => 'All translation',
+    'filters.option.not_translated' => 'For translation',
+    'filters.option.translated' => 'Translated',
 ];
